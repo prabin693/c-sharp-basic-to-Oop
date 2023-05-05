5ebfaaf0c1b6b2a992adda2aa5575e2bee927a27
Prabin Neupane <neupaneprabin693@gmail.com>
2023-05-05 20:52:08 +0545

    properties
    This all about c# properties

---
+++ Properties/Program.cs
@@ -0,0 +1,44 @@
+﻿//WAP in C# to demonstrate concept of Property.
+
+using System;
+
+public class Person
+{
+    private string _name;
+    private int _age;
+
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+
+    public int Age
+    {
+        get { return _age; }
+        set { _age = value; }
+    }
+
+    public Person(string name, int age)
+    {
+        _name = name;
+        _age = age;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Person person = new Person("John", 30);
+        Console.WriteLine("Name: " + person.Name);
+        Console.WriteLine("Age: " + person.Age);
+
+        person.Name = "Jane";
+        person.Age = 25;
+        Console.WriteLine("Name: " + person.Name);
+        Console.WriteLine("Age: " + person.Age);
+    }
+}
+
+
